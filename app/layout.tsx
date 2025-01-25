b5f388ec8bc6ea5ce3ad661df1450756f5160493
Tonči Marinac <toncimarinac@gmail.com>
2025-01-25 16:22:11 +0100

    google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,22 +20,37 @@ export const metadata: Metadata = {
   description: "Track your bills and payments easily.",
 };
 
+import Script from 'next/script'
+
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
     <html lang="en">
-      {" "}
-      {/* Hardcode the language if needed */}
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <head>
+        <Script
+          strategy="lazyOnload"
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
+        />
+        <Script
+          id="google-analytics"
+          strategy="lazyOnload"
+        >
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}');
+          `}
+        </Script>
+      </head>
+      <body>
         <Navbar />
         {children}
         <Analytics />
       </body>
     </html>
-  );
+  )
 }
